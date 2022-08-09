30a6f97988ad641e22209a7c828f595ac031e8f0
AmrMaged-H <90940335+AmrMaged-H@users.noreply.github.com>
2022-08-09 13:49:29 +0200

    Parity Is Parameterized

--- UART Tx/parity_calc.v
+++ UART Tx/parity_calc.v
@@ -9,9 +9,9 @@ module parity_calc (input wire [`WIDTH-1:0] P_DATA,
 
     
     always @(posedge CLK) begin
-        if (parity_type && Data_Valid) // Even Parity
+        if ( (parity_type == `EVEN_PARITY_CONFIG) && Data_Valid) // Even Parity
             par_bit <= ^P_DATA;
-        else if (!parity_type && Data_Valid) // Odd Parity
+        else if ((parity_type == `ODD_PARITY_CONFIG) && Data_Valid) // Odd Parity
             par_bit <= ~(^P_DATA);
     end
         
